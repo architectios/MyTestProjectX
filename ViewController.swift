2b71f1646876da7aff5524a692884e28656dbdc3
Emir Yalçın <emirryalcin@gmail.com>
2026-01-08 00:42:43 +0300

    viewController adjusted

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTestProjectX
+//
+//  Created by emir yalçın on 8.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+    let myArray = [10,20,30,40,50]
+        
+        for num in myArray{
+            print(num)
+        }
+    }
+
+
+}
+
